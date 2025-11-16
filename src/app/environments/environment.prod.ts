7f9c919056b346e816402f28c781efd19d426a64
TomasElia35 <eliagimeneztomas@gmail.com>
2025-11-16 11:27:14 -0300

    1

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  // 👇 Pega aquí la URL 'httpss://...' que te dio ngrok
   apiUrl: 'https://jacquelyne-unexaggeratory-craggedly.ngrok-free.dev'
 };
